f2ec0eebfd2b57281d7a7a59329bab0150e7aa90
adogins <124842665+adogins@users.noreply.github.com>
2024-07-30 16:47:56 -0400

    Updated the mission statement on the About page.
    Made changes as listed on figma to mission statement on the About page. Also changed the title on the about page from "About Us" to "More About Us" as shown on figma. Changed the devdog logo image to the demo image as shown on figma. Changed the font size under p (paragraph) from 1.2rem to 1.5rem on the globals.css sheet so the text better fits the webpage. Also changed changed the background-size on the globals.css sheet from 24% to 35% to better match the design shown on figma.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image"
 import mockup from "../images/mockup.png"
-import name from "../images/logo.png"
+import name from "../images/demo.png"
 
 const AboutPage = () => {
     return (
         <div className="section section-about">
             <div id="abouttitle">
-                <h1>About Us</h1>
+                <h1>More <b className="dark_red-bold">About Us</b></h1>
             </div>
 
             <div className="grid">
@@ -14,7 +14,7 @@ const AboutPage = () => {
                     <h1 id="whoweare"> <b className="dark_red-bold">Who </b>We Are</h1>
                     <p>DevDogs is a student-run club at UGA dedicated to benefiting our community through code. Each year, we work together to develop impactful software from concept to completion, learning real-world skills and industry-standard tech along the way. </p>
                 </div>
-                <Image id="about-us-mascot" src={name} alt="DevDogs Mascot" />
+                <Image id="demo" src={name} alt="ACM OSP Demo" />
             </div>
 
             <br />
@@ -22,9 +22,9 @@ const AboutPage = () => {
             <div className="grid">
                 <div>
                     <h1 id="whoweare">Our <b className="dark_red-bold">Mission</b></h1>
-                    <p>Above all, we aim to provide a space for passionate student developers to take ownership of meaningful projects from concept to completion.</p>
+                    <p>Above all, we aim to provide a space for passionate students from all disciplines to take ownership of meaningful software projects.</p>
                     <br />
-                    <p>In the process, we hope to provide our members the local, personal, and technical tools necessary to incite change of their own, one project at a time.</p>
+                    <p>In the process, we hope to build a network of awesome people, refine skills for our career, and learn how to incite change of our own, one project at a time.</p>
                 </div>
                 <Image id="mockup" src={mockup} alt="ACM OSP Mockup" width="650" height="325" />
             </div>
