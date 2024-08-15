101bacca2dc2b5217a886ea64b2d5ff67bc2b4e0
adogins <124842665+adogins@users.noreply.github.com>
2024-08-15 12:58:13 -0400

    Added the Our Impact section to the About Page
    Added buttons below the carousel. Added a title for the Impact section. Added the button grid to the global.css file. Added some styles to the global.css file related to the impact section.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,12 +3,15 @@ import mockup from "../images/mockup.png";
 import name from "../images/logo.png";
 import Carousel from "../components/Carousel";
 import demo from "../images/demo.png"
+import Button from "../components/Button"
+import Link from "next/link"
 
 //import image1 from "/carousel/carousel1.png"
 //import image2 from "../images/carousel/carousel2.jpg"
 //import image3 from "../images/carousel/carousel3.jpg"
 
-
+// The link for the "All Projects" button goes to the discord because there is not a projects page yet. 
+// The link for the "Our People" buttongoes to the Team page until the our People page is complete.
 
 const AboutPage = () => {
     const imagess = ["https://www.fisheries.noaa.gov/s3//styles/original/s3/2023-06/750x500-Great-White-iStock.jpg?itok=RWsvCGC6", 
@@ -20,35 +23,53 @@ const AboutPage = () => {
     return (
         <div>
             <div className="section section-about">
-            <div id="abouttitle">
-                <h1>More <b className="dark_red-bold">About Us</b></h1>
-            </div>
-
-            <div className="grid">
-                <div>
-                    <h1 id="whoweare"> <b className="dark_red-bold">Who </b>We Are</h1>
-                    <p>DevDogs is a student-run club at UGA dedicated to benefiting our community through code. Each year, we work together to develop impactful software from concept to completion, learning real-world skills and industry-standard tech along the way. </p>
+                <div id="abouttitle">
+                    <h1>More <b className="dark_red-bold">About Us</b></h1>
                 </div>
-                <Image id="demo" src={demo} alt="ACM OSP Demo" />
-            </div>
 
-            <br />
+                <div className="grid">
+                    <div>
+                        <h1 id="whoweare"> <b className="dark_red-bold">Who </b>We Are</h1>
+                        <p>DevDogs is a student-run club at UGA dedicated to benefiting our community through code. Each year, we work together to develop impactful software from concept to completion, learning real-world skills and industry-standard tech along the way. </p>
+                    </div>
+                    <Image id="demo" src={demo} alt="ACM OSP Demo" />
+                </div>
 
-            <div className="grid">
-                <div>
-                    <h1 id="whoweare">Our <b className="dark_red-bold">Mission</b></h1>
-                    <p>Above all, we aim to provide a space for passionate students from all disciplines to take ownership of meaningful software projects.</p>
+                <br />
+
+                <div className="grid">
+                    <div>
+                        <h1 id="whoweare">Our <b className="dark_red-bold">Mission</b></h1>
+                        <p>Above all, we aim to provide a space for passionate students from all disciplines to take ownership of meaningful software projects.</p>
+                        <br />
+                        <p>In the process, we hope to build a network of awesome people, refine skills for our career, and learn how to incite change of our own, one project at a time.</p>
+                    </div>
+                    <Image id="mockup" src={mockup} alt="ACM OSP Mockup" width="650" height="325" />
+                </div>
+
+                <br />
+                <br />
+                <br />
+
+                <div className="impact-section">
+                    <h1 id="impact">Our <b className="dark_red-bold">Impact</b></h1>
                     <br />
-                    <p>In the process, we hope to build a network of awesome people, refine skills for our career, and learn how to incite change of our own, one project at a time.</p>
+                    <Carousel images={images} />
+                    <br />
+                    <br />
+                    <h1 id="impact">More Info</h1>
+                    <br />
+                    <div className="button-grid">
+                        <Link href="https://discord.com/invite/MuyJ4f5xKE">
+                            <Button id="all-projects">All Projects</Button>
+                        </Link>
+                        <br />
+                        <Link href="\our-team">
+                            <Button id="our-people">Our People</Button>
+                        </Link>   
+                    </div>
                 </div>
-                <Image id="mockup" src={mockup} alt="ACM OSP Mockup" width="650" height="325" />
-            </div>
-
-            <br />
-            <br />
-            <br />
-            <Carousel images={images} />
-
+            
             </div>
            
         </div>
