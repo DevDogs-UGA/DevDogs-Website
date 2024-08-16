fabac45af0ea468c8ef543995f1fc2558765fd59
Gabe Livengood <gabe@ggliv.com>
2024-08-16 11:16:14 -0400

    Change instances of 'class' to 'className'

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -7,7 +7,7 @@ const Card = (props) => {
             <Image id="pic" src={props.image} width="400" height="450" />
             <div id="officer-description">
                 <br />
-                <h2 class="officer-name">{props.name}</h2>
+                <h2 className="officer-name">{props.name}</h2>
                 <p id="officer-title-name">{props.title}</p>
                 <br />
             </div>
