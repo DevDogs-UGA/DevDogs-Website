cdcc5f32ac37b8e99213fb98b81355e9fe5fb57b
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-08-18 20:46:09 -0400

    Updated mobile view styling

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,7 @@
 const Page = () => {
     return (
         <div>
-            <h3 className=" my-[35%] text-[3rem]">COMING SOON!</h3>
+            <h3 className=" my-[35%] text-[5rem]">COMING SOON!</h3>
         </div>
     )
 }
