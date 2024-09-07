91707f3bd1509d4fd673a592e1393867fdf00977
adogins <124842665+adogins@users.noreply.github.com>
2024-09-07 17:39:20 -0400

    Changed formatting

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -54,7 +54,7 @@ const AboutPage = () => {
             </p>
           </div>
           <div className="basis-1/2 py-10">
-            <Image 
+            <Image
               src={devdogswelcome}
               alt="DevDogs Welcome"
             />
