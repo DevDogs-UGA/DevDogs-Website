2401acbc8e553709cdf98509d76fd00def697a46
Jay Patel <81924044+jayp822@users.noreply.github.com>
2024-10-20 19:51:30 -0400

    Revert "done"
    This reverts commit b4ac7e6d9dc285a947c0d179f870705ebe3a08d5.

--- src/app/about/events/page.js
+++ src/app/about/events/page.js
@@ -1,3 +1,4 @@
+import PageTitleTemplate from "@/app/components/PageTitleTemplate";
 import Link from "next/link";
 import EmblaCarousel from "@/app/components/EmblaCarousel";
 import Caoursel1 from "@/app/images/carousel1.png";
