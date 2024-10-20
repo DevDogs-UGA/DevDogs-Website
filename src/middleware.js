68161e755092d72fc8a8b914a56f697342b2ca76
Reeyan Khimani <reeyank@Reeyans-MBP.attlocal.net>
2024-10-20 13:50:37 -0400

    Fix to router issue for dashboard page

--- src/middleware.js
+++ src/middleware.js
@@ -5,25 +5,26 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const cookie = await req.cookies.get("connect.sid");
 
-  const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-      Cookie: `${await cookie?.name}=${await cookie?.value}`,
-    },
-  });
-  console.log(res, res.status);
+  if (req.cookies.has("connect.sid")) {
+    const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `${await cookie?.name}=${await cookie?.value}`,
+      },
+    });
 
-  if (res.status !== 200) {
-    return NextResponse.redirect(new URL("/login", req.url));
+    if (res.status !== 200) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
   }
 
   // Allow the request to proceed
   return NextResponse.next();
 }
 
-// Apply middleware to specific routes (e.g., protect /dashboard)
+// Apply middleware to specific routes
 export const config = {
   matcher: ["/dashboard/:path*"], // Routes that need authentication
 };
