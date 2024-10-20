484045b6240ffa7ef967bc514273b59440aec049
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-10-20 19:59:20 -0400

    Revert "Fix to router issue for dashboard page"
    This reverts commit 9bba89da385f58fa5676a47e5150cb9160c2950e.

--- src/middleware.js
+++ src/middleware.js
@@ -5,26 +5,25 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const cookie = await req.cookies.get("connect.sid");
 
-  if (req.cookies.has("connect.sid")) {
-    const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `${await cookie?.name}=${await cookie?.value}`,
-      },
-    });
+  const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `${await cookie?.name}=${await cookie?.value}`,
+    },
+  });
+  console.log(res, res.status);
 
-    if (res.status !== 200) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
+  if (res.status !== 200) {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
   // Allow the request to proceed
   return NextResponse.next();
 }
 
-// Apply middleware to specific routes
+// Apply middleware to specific routes (e.g., protect /dashboard)
 export const config = {
   matcher: ["/dashboard/:path*"], // Routes that need authentication
 };
