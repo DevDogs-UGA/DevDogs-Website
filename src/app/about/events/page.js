8c77de2c4f7674a658c966c2de3be8ed640375d4
Jay Patel <81924044+jayp822@users.noreply.github.com>
2024-10-20 18:43:20 -0400

    done

--- src/app/about/events/page.js
+++ src/app/about/events/page.js
@@ -1,4 +1,24 @@
+import PageTitleTemplate from "@/app/components/PageTitleTemplate";
+import Link from "next/link";
+import EmblaCarousel from "@/app/components/EmblaCarousel";
+import Caoursel1 from "@/app/images/carousel1.png";
+import Caoursel2 from "@/app/images/carousel2.jpg";
+import Caoursel3 from "@/app/images/carousel3.jpg";
+
 const EventsPage = () => {
+  const OPTIONS = { loop: true };
+  const imagesForCarousel = [
+    {
+      image: Caoursel1,
+    },
+    {
+      image: Caoursel2,
+    },
+    {
+      image: Caoursel3,
+    },
+  ];
+
   return (
     <div className="section section-event page-main-side-padding">
       <div className="text-center my-[125px]">
@@ -14,66 +34,78 @@ const EventsPage = () => {
         </h2>
       </div>
 
-      <div className="text-center">
-        <h1 className="font-semibold text-[2.5rem] text-MidnightBlue">
-          All students are welcome!
-        </h1>
-
-        <p className="mx-[10%]">
-          Get an amazing experience and a glimpse into what working in the
-          software development industry is like!
-        </p>
+      <div className="flex gap-6 pb-10 items-center px-4">
+        <div className="text-left">
+          <h2 className="font-bold inline text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] text-MidnightBlue">
+            General Body Meetings (
+          </h2>
+          <h2 className="font-bold inline text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] text-BulldogRed">
+            GBM
+          </h2>
+          <h2 className="font-bold inline text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] text-MidnightBlue">
+            s)
+          </h2>
+          <p className="text-[1rem] sm:text-[1.3rem] md:text-[1.5rem] ">
+            Key information meetings for DevDogs. Attendees will have the
+            opportunity to meet other members, learn about the club, and learn
+            about our software project this year. All are welcome and no RSVP is
+            required.
+          </p>
+        </div>
+        <div className="text-right ">
+          <h2 className="font-bold inline text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] text-BulldogRed">
+            Dev{" "}
+          </h2>
+          <h2 className="font-bold inline text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] text-MidnightBlue">
+            Sessions
+          </h2>
+          <p className="text-[1rem] sm:text-[1.3rem] md:text-[1.5rem]">
+            Collaborative project development sessions for DevDogs. The primary
+            meeting type. Attendees will discuss the progress of our project,
+            discuss next steps, and tackle development problems together
+            in-person. All are welcome and no RSVP is required.
+          </p>
+        </div>
       </div>
 
-      <h1 className="font-bold text-[2rem] md:[2.5rem] mt-8 text-MidnightBlue">
-        General Body{" "}
-        <p className="font-bold text-[2rem] md:[2.5rem] inline text-BulldogRed">
-          Meetings
-        </p>
-      </h1>
-
-      <p>Monday August 26th, 2024 from 5:30 PM to 6:30PM in MLC 150</p>
-      <p>Monday September 9th, 2024 from 5:30 PM to 6:30PM in MLC 150</p>
-
-      <h1 className="font-bold text-[2rem] md:[2.5rem] mt-8 text-MidnightBlue">
-        Dev{" "}
-        <p className="font-bold text-[2rem] md:[2.5rem] inline text-BulldogRed">
-          Sessions
-        </p>
-      </h1>
-
-      <p>Every Monday from 5:30 PM to 6:30PM in (New) Poultry Science 125*</p>
-      <p>
-        <i>*Begins on September 16th, 2024</i>
+      <p className="text-center py-2 text-[1rem] sm:text-[1.3rem] md:text-[1.5rem] px-4">
+        The latest dates and times can be found in the calendar below. Members
+        are welcome to attend our livestreams on{" "}
+        <Link href="tps://discord.com/invite/MuyJ4f5xKE" target="_blank">
+          Discord
+        </Link>{" "}
+        if they cannot attend in-person.
       </p>
 
-      <h1 className="font-bold text-[2rem] sm:text-[2rem] md:[2.5rem] mt-8 text-MidnightBlue">
-        Upcoming{" "}
-        <p className="font-bold text-[2rem] sm:text-[2rem] md:[2.5rem] inline text-BulldogRed">
-          Meetings
-        </p>
-      </h1>
+      <div className="py-10 text-center">
+        <h2 className="font-bold inline text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] text-BulldogRed">
+          Special{" "}
+        </h2>
+        <h2 className="font-bold inline text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] text-MidnightBlue">
+          Events
+        </h2>
 
-      <p>
-        <b>DevDogs: Ready, Steady, Launch! - August 26th, 2024 </b>
-      </p>
-      <p>
-        Join us to learn more about DevDogs, meet new people, and how you can
-        get involved!
-      </p>
+        <EmblaCarousel
+          slides={imagesForCarousel}
+          banner={false}
+          options={OPTIONS}
+        />
+      </div>
 
-      <iframe
-        id="iframe-test"
-        src="https://embed.styledcalendar.com/#IvBYsu9dkaqY2qyAP7tR"
-        title="Styled Calendar"
-        className="styled-calendar-container mt-8"
-        data-cy="calendar-embed-iframe"
-      ></iframe>
-      <script
-        async
-        type="module"
-        src="https://embed.styledcalendar.com/assets/parent-window.js"
-      ></script>
+      <div>
+        <iframe
+          id="iframe-test"
+          src="https://embed.styledcalendar.com/#IvBYsu9dkaqY2qyAP7tR"
+          title="Styled Calendar"
+          className="styled-calendar-container mt-8"
+          data-cy="calendar-embed-iframe"
+        ></iframe>
+        <script
+          async
+          type="module"
+          src="https://embed.styledcalendar.com/assets/parent-window.js"
+        ></script>
+      </div>
     </div>
   );
 };
