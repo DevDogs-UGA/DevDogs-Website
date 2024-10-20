2f38ee473979560e7bb2871a7bbf70e045d31a23
Reeyan Khimani <reeyank@Reeyans-MBP.attlocal.net>
2024-10-20 13:26:36 -0400

    Fix to router issue for dashboard page

--- src/middleware.js
+++ src/middleware.js
@@ -5,35 +5,22 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const cookie = await req.cookies.get("connect.sid");
 
-  // Check if the cookie exists
-  if (!cookie) {
-    console.log("Cookie not found, redirecting to login");
+  const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: cookie?.name + "=" + cookie?.value,
+    },
+  });
+  console.log(res, res.status);
+
+  if (res.status !== 200) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  try {
-    const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `${cookie.name}=${cookie.value}`,
-      },
-    });
-
-    console.log(res, res.status);
-
-    if (res.status !== 200) {
-      console.log("Authentication failed, redirecting to login");
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-
-    // Allow the request to proceed
-    return NextResponse.next();
-  } catch (error) {
-    console.error("Error during authentication:", error);
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // Allow the request to proceed
+  return NextResponse.next();
 }
 
 // Apply middleware to specific routes (e.g., protect /dashboard)
