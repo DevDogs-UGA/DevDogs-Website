d48fe89aa386aa5cfad98e79bb680e65c888c2bf
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-10-20 19:59:04 -0400

    Revert "Fix to router issue for dashboard page"
    This reverts commit 27c4018117694ceaaf412199e7bc96f28a825b47.

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -310,7 +310,7 @@ const Page = () => {
       sessionStorage.setItem("email", email);
       sessionStorage.setItem("access_token", await data.access_token);
       router.refresh();
-      window.location.href = "/dashboard";
+      router.push("/dashboard");
     } catch (error) {
       setShowing(true);
       console.error("Login failed:", error);
