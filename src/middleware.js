055021d53f688fc09f07099ce6206a86e143fb70
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-10-20 20:04:42 -0400

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,6 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const cookie = req.cookies.get("connect.sid");
 
-<<<<<<< HEAD
   const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
     method: "GET",
     credentials: "include",
@@ -14,7 +13,8 @@ export async function middleware(req) {
       Cookie: cookie?.name + "=" + cookie?.value,
     },
   });
-=======
+
+
   if (req.cookies.has("connect.sid")) {
     const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
       method: "GET",
@@ -24,7 +24,6 @@ export async function middleware(req) {
         Cookie: `${await cookie?.name}=${await cookie?.value}`,
       },
     });
->>>>>>> parent of dbba81d2 (Fix to router issue for dashboard page)
 
   if (!res.ok) {
     return NextResponse.redirect(new URL("/login", req.url));
