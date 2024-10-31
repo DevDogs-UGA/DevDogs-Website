68b28137a10d768d0a334d41acc1df51de916bfe
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-10-31 07:00:01 -0400

    Revert "Update middleware.js"
    This reverts commit 055021d53f688fc09f07099ce6206a86e143fb70.

--- src/middleware.js
+++ src/middleware.js
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const cookie = req.cookies.get("connect.sid");
 
+<<<<<<< HEAD
   const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
     method: "GET",
     credentials: "include",
@@ -13,8 +14,7 @@ export async function middleware(req) {
       Cookie: cookie?.name + "=" + cookie?.value,
     },
   });
-
-
+=======
   if (req.cookies.has("connect.sid")) {
     const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
       method: "GET",
@@ -24,6 +24,7 @@ export async function middleware(req) {
         Cookie: `${await cookie?.name}=${await cookie?.value}`,
       },
     });
+>>>>>>> parent of dbba81d2 (Fix to router issue for dashboard page)
 
   if (!res.ok) {
     return NextResponse.redirect(new URL("/login", req.url));
