64883926af754f44988a14b4e2a2b98b06195ecb
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-10-31 06:59:47 -0400

    Revert "Reapply "Fix to router issue for dashboard page""
    This reverts commit ce49d365e446aae9bdcab85b8e5e3e0974e7d532.

--- src/middleware.js
+++ src/middleware.js
@@ -15,13 +15,13 @@ export async function middleware(req) {
   });
 
 
-  if (cookie) {
+  if (req.cookies.has("connect.sid")) {
     const res = await fetch("https://api.devdogs.uga.edu/auth/session", {
       method: "GET",
       credentials: "include",
       headers: {
         "Content-Type": "application/json",
-        Cookie: `${cookie.name}=${cookie.value}`,
+        Cookie: `${await cookie?.name}=${await cookie?.value}`,
       },
     });
 
