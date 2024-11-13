ea58d8f308644011beee09d13a8a8b62820ff987
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-11-12 19:05:13 -0500

    Changed button colors

--- src/app/about/projects/page.js
+++ src/app/about/projects/page.js
@@ -97,7 +97,7 @@ const Page = () => {
 
       <Link
         href="https://github.com/DevDogs-UGA/Optimal-Schedule-Builder"
-        className="red container mx-auto my-[75px] block max-w-[17rem] rounded-full py-3 font-semibold text-white transition duration-300 ease-in-out"
+        className="bg-BulldogRed container transition ease-in-out duration-300 block mx-auto rounded-full max-w-[17rem] font-semibold py-3 my-[75px]  text-white hover:bg-black hover:text-white"
       >
         <Button className="">View</Button>
       </Link>
